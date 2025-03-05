cde15516e10a59973ff82399199dfbee34011cc4
TrilhaX <joaotrilha01@gmail.com>
2025-03-05 09:20:13 -0300

    Inicializando JS

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-a=1
-b=2
+//pegar o elemento canvas
+//inicializar o canvas
+//cria a função loop
+
+const canvas = document.querySelector("jogo2D")
+const ctx = canvas.getContext("2d")
+
+function loop(){
+    
+}
+loop()
